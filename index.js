a3216cac5d4a2aba033daee67b2cf3a689f01162
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-05-03 19:57:07 +0600

    server setup done

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const fkdata = require('./Data/data.json');
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send(fkdata);
+});
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`SM shop run ${port}`)
 })
